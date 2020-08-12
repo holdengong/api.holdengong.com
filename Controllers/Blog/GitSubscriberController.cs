202e7c65bc133031e4711232e62a08925fa933fb
gongyingtao <373317270@qq.com>
2020-08-12 14:07:10 +0800

    回调测试接口

--- Controllers/Blog/GitSubscriberController.cs
+++ Controllers/Blog/GitSubscriberController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace api.holdengong.com.Controllers
 {
@@ -11,6 +14,16 @@ namespace api.holdengong.com.Controllers
         public IActionResult GitSubscriber([FromBody]object request)
         {
             Process.Start("/scripts/blog.sh");
+            return Ok();
+        }
+
+        [HttpPost("api/callback")]
+        public IActionResult CallbackAsync([FromBody]object request)
+        {
+            string json = JsonConvert.SerializeObject(request);
+            string path = Path.Combine(Environment.CurrentDirectory, "callback.json");
+            System.IO.File.WriteAllText(path, json);
+
             return Ok();
         }
     }
